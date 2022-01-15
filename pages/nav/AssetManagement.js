4797a09bc8d8eea59f3cd7c37ae6f65ed985dd71
yuri0411 <yu_ri0411@naver.com>
2022-01-16 01:46:40 +0900

    자산관리 페이지 레이아웃 배치

--- pages/nav/AssetManagement.js
+++ pages/nav/AssetManagement.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Tabs, PageHeader, Button, List } from 'antd'
 
 const AssetManagement = () => {
-    return (
-        <div>
-            자산 관리
-        </div>
+  const { TabPane } = Tabs;
+
+  return (
+    <Tabs tabPosition="left">
+      <TabPane tab="계좌/현금" key="1">
+        <PageHeader
+          className="site-page-header"
+          title="계좌/현금"
+          backIcon={false}
+          extra={
+            <Button key="1" type="link">
+              추가
+            </Button>
+          }
+        />
+        <List
+          header={<div>입출금 <span>0 원</span></div>}
+          size="small"
+          renderItem={item => (
+            <List.Item>
+
+            </List.Item>
+          )}
+        />
+      </TabPane>
+      <TabPane tab="카드" key="2">
+        Content of Tab 2
+      </TabPane>
+      <TabPane tab="대출" key="3">
+        Content of Tab 3
+      </TabPane>
+      <TabPane tab="카테고리" key="4">
+        Content of Tab 3
+      </TabPane>
+    </Tabs>
     );
 };
 
