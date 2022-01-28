df3b027d98b29150936b1661e7fa58babe9e1edf
yuri0411 <yu_ri0411@naver.com>
2022-01-29 01:16:08 +0900

    nav 컴포넌트 분리

---
+++ components/NavBar.js
@@ -0,0 +1,37 @@
+import { Menu } from 'antd'
+import Link from 'next/link'
+import useSWR from 'swr'
+import api from '../constants/api'
+import { useRouter } from 'next/router'
+
+const fetcher = (url) => fetch(url).then((r) => r.json())
+
+const categoryComponent = {
+  1: 'assetStatus',
+  2: 'accountBook',
+  3: 'assetManagement',
+}
+
+const NavBar = () => {
+  const { data, error } = useSWR(`${api.url}/categories`, fetcher)
+  const router = useRouter()
+  console.log('router', router)
+  return (
+    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      {data?.map((category) => (
+        <Menu.Item key={category.id}>
+          <Link
+            href={{
+              pathname: `/nav/${categoryComponent[category.id]}`,
+              query: { parentId: category.id },
+            }}
+          >
+            <a>{category.name}</a>
+          </Link>
+        </Menu.Item>
+      ))}
+    </Menu>
+  )
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Col, Layout, Menu, Row } from 'antd'
 import Link from 'next/link'
 import 'antd/dist/antd.css'
 import '../styles/globals.css'
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
   const { Header, Content } = Layout
@@ -13,23 +14,7 @@ function MyApp({ Component, pageProps }) {
             <div className="logo"> zu:money </div>
           </Col>
           <Col span={14}>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-              <Menu.Item key="Asset Status">
-                <Link href="/nav/assetStatus">
-                  <a>자산 현황</a>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="Account Book">
-                <Link href="/nav/accountBook">
-                  <a>가계부</a>
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="Asset Management">
-                <Link href="/nav/assetManagement">
-                  <a>자산 관리</a>
-                </Link>
-              </Menu.Item>
-            </Menu>
+            <NavBar />
           </Col>
           <Col span={4}>
             <Link href="/logIn">
